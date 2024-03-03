483f9b65ee0f00edf67c52c43f33369927f0089d
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-03 22:59:23 +0530

    update code file

--- 17multi_dimensionarray.php
+++ 17multi_dimensionarray.php
@@ -1,5 +1,6 @@
 <?php
 // learning multi dimension array today in video 19
+
 // creating a 2 dimensional array
 // $multiDin = array(array(2,5,7,8),
 //                   array(1,2,3,1),
@@ -9,30 +10,33 @@
 // echo $multiDin[2][0];
 
 // printing the contents of a 2 dimensional array
-// for ($i=0; $i <count($multiDin) ; $i++) { 
-//     echo var_dump($multiDin[$i]);
-//     echo "<br>";
-// }
+/*
+for ($i=0; $i <count($multiDin) ; $i++) { 
+    echo var_dump($multiDin[$i]);
+    echo "<br>";
+} */
 
 // printing values in matrix form with nested loop
-// for ($i=0; $i <count($multiDin); $i++) {
-//     for ($j=0; $j <count($multiDin[$i]) ; $j++) { 
-//          echo $multiDin[$i][$j];
-//          echo " ";
-//     }
-//     echo "<br>";
-// }
+/*
+for ($i=0; $i <count($multiDin); $i++) {
+    for ($j=0; $j <count($multiDin[$i]) ; $j++) { 
+         echo $multiDin[$i][$j];
+         echo " ";
+    }
+    echo "<br>";
+} */
 
 // printing 3 dimension array
+
 /* $multiDin = array(array(array(10, 20, 30, 40),
-                        array(50, 60, 70, 80),
-                        array(90, 10, 11, 12)),
-                  array(array(13, 14, 15, 16),
-                        array(17, 18, 19, 20),
-                        array(21, 22, 23, 24)),      
-                  array(array(25, 26, 27, 28),
-                        array(29, 30, 31, 32),
-                        array(33, 34, 35, 36))); */
+                           array(50, 60, 70, 80),
+                           array(90, 10, 11, 12)),
+                     array(array(13, 14, 15, 16),
+                           array(17, 18, 19, 20),
+                           array(21, 22, 23, 24)),      
+                     array(array(25, 26, 27, 28),
+                           array(29, 30, 31, 32),
+                           array(33, 34, 35, 36))); */
 
 // echo $multiDin[2][2][0];
 
@@ -78,6 +82,7 @@ $multiDin = array(array(array(array(0,0,0,0),
                               array(0,0,1,1))));
 
 // echo $multiDin[0][0][0][0];
+
 for ($i=0; $i <count($multiDin); $i++){
     for ($j=0; $j <count($multiDin[$i]); $j++){
         for ($k=0; $k <count($multiDin[$i][$j]); $k++){
