c87fc9f8159dd7960256a93f2f039bf19caf1522
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-04 23:01:19 +0530

    edit code

--- 18scopes.php
+++ 18scopes.php
@@ -6,7 +6,8 @@ $b = 78;
 $c = 99;
 
 function printValue(){
-    global $a, $b; // give me the access to the global variable
+    // global $a, $b; // give me the access to this global variable now here you can change or update the value of global variable 
+    // uncomment the above global variable and check the difference
     $a = 100;  // this is a local variable
     $b = 1000;
     echo "the value of your variable a is $a and b is $b";
@@ -19,7 +20,7 @@ printValue();
 echo "<br> the value of your variable a is $a and b is $b";
 echo "<br>";
 
-// echo var_dump($GLOBALS);
+// echo var_dump($GLOBALS); // will print all the global variables
 echo var_dump($GLOBALS["c"]);
 echo var_dump($GLOBALS["a"]);
 
