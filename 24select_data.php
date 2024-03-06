53faa90e66395db9d3c949c64c5aa7b33f706bf8
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-06 22:31:10 +0530

    php files

--- 24select_data.php
+++ 24select_data.php
@@ -25,12 +25,13 @@ echo " records found in the database <br>";
 
 // display the rows returned by the sql query
 if($num>0){
-    // $row = mysqli_fetch_assoc($result);
-    // echo var_dump($row);
-    // echo "<br>";
-    // $row = mysqli_fetch_assoc($result);
-    // echo var_dump($row);
-    // echo "<br>";
+    /*
+    $row = mysqli_fetch_assoc($result);
+    echo var_dump($row);
+    echo "<br>";
+    $row = mysqli_fetch_assoc($result);
+    echo var_dump($row);
+    echo "<br>"; */
 
     // we can fetch in a better way using the while loop
     while($row = mysqli_fetch_assoc($result)){
