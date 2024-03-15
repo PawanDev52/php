40b41ad139cd757ca8695a5e47117ef85aba32d8
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-15 23:01:04 +0530

    php file

--- 26delete.php
+++ 26delete.php
@@ -7,7 +7,6 @@ $password = "";
 $database = "pawandev";
 
 $connection = mysqli_connect($servername, $username, $password, $database);
-
 if(!$connection){
     die("we failed to connect to the database: ". mysqli_connect_error());
 }
