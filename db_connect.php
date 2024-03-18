07a480502e6e7ed830c343e6046a517090aa92ab
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-18 22:43:55 +0530

    php file

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,11 @@
 <?php
+// connecting to database
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "pawandev";
 
+// creating a connection
 $connection = mysqli_connect($servername, $username, $password, $database);
 if(!$connection){
     die("we failed to connect to the database: ". mysqli_connect_error());
