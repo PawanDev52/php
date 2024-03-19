f451a8cc1aa7082b9eea1a70fbb069da75227b19
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-19 22:56:56 +0530

    php file

--- 27require_include.php
+++ 27require_include.php
@@ -1,15 +1,12 @@
 <?php
 // learning require and include in video 33
 
-// include is used to include a file 
-// if there is any error in that file still this code will execute!
-// it will show only warning
+// include is used to include a file, if there is any error in that file still this code will execute! it will show only warning.
 // include 'db_connect.php';
 
-// in require - it makes required the condition otherwise code will not execute 
-// it will not only show warning but error also
+// In require - it makes required the condition otherwise code will not execute, it will not only show warning but error also.
 require 'db_connect.php';
-echo "this file";
+
 
 
 ?>
