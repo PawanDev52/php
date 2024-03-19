310e43afd52dc55ee834cae3653266bb79293437
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-19 22:53:14 +0530

    php file

--- 27require_include.php
+++ 27require_include.php
@@ -3,8 +3,12 @@
 
 // include is used to include a file 
 // if there is any error in that file still this code will execute!
-include 'db_connect1.php'; 
-// require 'db_connect.php';
+// it will show only warning
+// include 'db_connect.php';
+
+// in require it makes required the condition otherwise code will not execute 
+// 
+require 'db_connect.php';
 echo "this file";
 
 
