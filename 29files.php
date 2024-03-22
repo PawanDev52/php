55c580b7f4cdbc010bc05fb8c87f4866078aebdc
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-22 22:58:19 +0530

    php file

--- 29files.php
+++ 29files.php
@@ -1,7 +1,10 @@
 <?php
 // learning open and close files in video 35
 
-$fptr = fopen("myfile.txt", "r");
-echo $fptr;
+$fptr = fopen("myfile1.txt", "r");
+echo var_dump($fptr);
+if(!$fptr){
+    die("Unable to open this file. Please enter a valid filename");
+}
 
 ?>
