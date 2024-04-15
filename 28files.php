4086c267a998b8301d97050ed4b6547cf14a3285
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-15 22:11:33 +0530

    php file

--- 28files.php
+++ 28files.php
@@ -5,6 +5,8 @@
 // echo $a; // if you read file like this it will reture the total no. of characters in the end.
 
 readfile("myfile.txt");
+
 // if you read file by this method it will not return total no. of characters in the end.
 // text file ki tarah html file ko bhi read kar sakte hai same method se.
+
 ?>
