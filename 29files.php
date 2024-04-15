11de4bf323eb90f7ae958ddb2f46d29ab04a6f4f
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-15 22:09:30 +0530

    php file

--- 29files.php
+++ 29files.php
@@ -1,9 +1,7 @@
 <?php
 // learning fopen and fclose and fread methods of files in video 35
-
 $fptr = fopen("myfile.txt", "r");
 // echo var_dump($fptr);
-
 if(!$fptr){
     die("Unable to open this file. Please enter a valid filename!");
 }
