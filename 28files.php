4392b947d5816de3180d42250f79194c54124d06
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-16 22:25:53 +0530

    php file

--- 28files.php
+++ 28files.php
@@ -1,6 +1,5 @@
 <?php
 // learning working of files in video 34
-
 // $a = readfile("myfile.txt"); // if you execute only this command then total no. of characters will not return.
 // echo $a; // if you read file like this it will reture the total no. of characters in the end.
 
