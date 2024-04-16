bc1692a8f6abd6b43c144e295a9fb9d51ed4d2d1
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-04-16 22:24:51 +0530

    php file

--- 29files.php
+++ 29files.php
@@ -5,7 +5,6 @@ $fptr = fopen("myfile.txt", "r");
 if(!$fptr){
     die("Unable to open this file. Please enter a valid filename!");
 }
-
 $content = fread($fptr, filesize("myfile.txt"));
 fclose($fptr);
 echo $content;
