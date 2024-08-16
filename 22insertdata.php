71b2de23f8e10e8426264095092fb08ac2d952d5
Pawan sharma <pawansh803@gmail.com>
2024-08-16 22:33:20 +0530

    php file

--- 22insertdata.php
+++ 22insertdata.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "pawandev";
+$database = "pawandev5";
 
 $cont = mysqli_connect($servername, $username, $password, $database);
 
