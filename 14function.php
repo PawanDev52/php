a76361df1fa443979e199f62efb1a33a2fb07d04
Pawan sharma <pawansh803@gmail.com>
2024-08-22 18:28:29 +0530

    php file

--- 14function.php
+++ 14function.php
@@ -1,6 +1,6 @@
 <?php
 // learning functions today in video 16
-
+/*
 function processMarks($marksArr){
     $sum = 0;
     foreach ($marksArr as $value){
@@ -8,11 +8,35 @@ function processMarks($marksArr){
     }
     return $sum;
 }
-
 $rohanDas = [34, 98, 45, 12, 98, 93];
 $sumMarks = processMarks($rohanDas);
 
 echo "Total marks scored by rohan out of 600 is $sumMarks <br>";
 echo $sumMarks;
+*/
+
+// Basic Function
+function heading()
+{
+    echo "<h3>User Details</h3>";
+}
+
+function displayDetails()
+{
+    echo heading();
+    echo "User name is demo <br/>";
+    echo "User age is 20 <br/>";
+    echo "User is working <br/>";
+    echo "<hr/>";
+}
+
+displayDetails();
+displayDetails();
+displayDetails();
+displayDetails();
+
+// ============================================================================================
+
+// function with parameter
 
 ?>
