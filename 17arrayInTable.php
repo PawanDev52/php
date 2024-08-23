3edaf64cccbf0f4dba36291f01c6dad379e4d2a0
Pawan sharma <pawansh803@gmail.com>
2024-08-23 16:59:11 +0530

    php file

--- 17arrayInTable.php
+++ 17arrayInTable.php
@@ -1,46 +1,28 @@
-<table border="1">
-    <tr>
-        <td>Sr. no.</td>
-        <td>Name</td>
-        <td>Age</td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>Peter</td>
-        <td>22</td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>Peter</td>
-        <td>22</td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>Peter</td>
-        <td>22</td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>Peter</td>
-        <td>22</td>
-    </tr>
-</table>
-
 <?php
 $users = [
-    [1, "peter", 26],
-    [2, "henry", 33],
-    [3, "paul", 30],
-    [4, "golly", 13],
-    [5, "dholu", 55]
+    [1, "peter ford", "japan"],
+    [2, "henry seth", "bihar"],
+    [3, "paul smith", "keral"],
+    [4, "golly bold", "rajasthan"],
+    [5, "dholu roy", "gujraat"]
 ];
 
-for ($i=0; $i < count($users); $i++) {
-    for ($j=0; $j < count($users[$i]); $j++) {
-        echo $users[$i][$j];
-        echo " ";
-    }
-    echo "<br>";
-}
+echo "<table border='1'>";
+echo "<tr>
+    <td>Sr. No.</td>
+    <td>Name</td>
+    <td>State</td>
+    </tr>";
 
+    for ($i=0; $i < count($users); $i++) {
+    echo "<tr>";
+    for ($j=0; $j < count($users[$i]); $j++) {
+        echo "<td>";
+        echo $users[$i][$j];
+        // echo " ";
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
 ?>
