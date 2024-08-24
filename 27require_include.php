5e9ae175f2d405370bb982aff46cf53e86dc9162
Pawan sharma <pawansh803@gmail.com>
2024-08-24 15:28:40 +0530

    php file

--- 27require_include.php
+++ 27require_include.php
@@ -2,10 +2,14 @@
 // learning require and include in video 33
 
 // include is used to include a file, if there is any error in that file still this code will execute! it will show only warning.
-// include 'db_connect.php';
+// include 'db_connect.php'; //method 1
+// include('db_connect.php'); //method 2 include can include a file multiple times but include once will include only one time.
+// include_once("./db_connect.php"); // include once if the file is included then do not include again.
+
 
 // In require - it makes required the condition otherwise code will not execute, it will not only show warning but error also.
-require 'db_connect.php';
+// require 'db_connect.php';
+require_once 'db_connect.php';
 
 $sql = "SELECT * FROM `newtable`";
 $result = mysqli_query($connection, $sql);
