946b5e18f40371458b0fcc356a5983466dd6bdbd
Pawan sharma <pawansh803@gmail.com>
2024-08-25 14:47:25 +0530

    php file

--- 29files.php
+++ 29files.php
@@ -1,11 +1,35 @@
 <?php
 // learning fopen and fclose and fread methods of files in video 35
-$fptr = fopen("myfile.txt", "r");
-// echo var_dump($fptr);
-if(!$fptr){
-    die("Unable to open this file. Please enter a valid filename!");
-}
-$content = fread($fptr, filesize("myfile.txt"));
-fclose($fptr);
-echo $content;
+// $fptr = fopen("myfile.txt", "r");
+// // echo var_dump($fptr);
+// if(!$fptr){
+//     die("Unable to open this file. Please enter a valid filename!");
+// }
+// $content = fread($fptr, filesize("myfile.txt"));
+// fclose($fptr);
+// echo $content;
 ?>
+
+<!-- ============================================================================= -->
+
+<?php
+// read files
+// $file = 'data.txt';
+// $myfile = fopen($file, 'r') or die('unable to read file');
+// echo fread($myfile, filesize($file));
+// fclose($myfile);
+
+
+if (isset($_FILES['file'])) {
+    $file = $_FILES['file']['tmp_name'];
+    $myfile = fopen($file, 'r') or die('unable to read file');
+    echo fread($myfile, filesize($file));
+    fclose($myfile);
+}
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <br><br>
+    <button>read file</button>
+</form>
--- 30file.php
+++ 30file.php
@@ -1,6 +1,18 @@
 <?php
 // learning files method - fgets and fgetc in php in video - 36
 
-$fptr = fopen("myfile.txt", "r");
-echo fgets($fptr);
+// $fptr = fopen("myfile.txt", "r");
+// echo fgets($fptr);
+?>
+
+<?php
+// list of files
+
+$path = "uploads";
+$items = scandir($path); // for scanning a directory
+$items = array_diff($items, array(".", "..")); // for removing same elements from the array.
+// print_r($items);
+foreach ($items as $item){
+    echo $item;
+}
 ?>
