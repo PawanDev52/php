436001c5be382ab1bd47d219dad110b59da2cbd2
Pawan sharma <pawansh803@gmail.com>
2024-08-25 14:19:18 +0530

    php file

--- 28files.php
+++ 28files.php
@@ -3,9 +3,38 @@
 // $a = readfile("myfile.txt"); // if you execute only this command then total no. of characters will not return.
 // echo $a; // if you read file like this it will reture the total no. of characters in the end.
 
-readfile("myfile.txt");
+// readfile("myfile.txt");
 
 // if you read file by this method it will not return total no. of characters in the end.
 // text file ki tarah html file ko bhi read kar sakte hai same method se.
 
+// =======================================================================================
+
+// create file
+// $filename = "dummy.txt";
+// $content = "this is a dummy file";
+// $file = fopen($filename, 'w') or die('unable to create file');
+// fwrite($file, $content);
+// fclose($file);
+// echo "file created";
+?>
+
+<form action="" method="post">
+  <input type="text" name="filename" placeholder="Enter file name">
+  <br /><br />
+  <textarea name="content"></textarea>
+  <br /><br />
+  <button name="button">Create file</button>
+</form>
+<?php
+// creating file with form
+
+if (isset($_POST['filename'])) {
+    $filename = $_POST['filename'];
+    $content = $_POST['content'];
+    $file = fopen($filename, 'w') or die('unable to create file');
+    fwrite($file, $content);
+    fclose($file);
+    echo "file created";
+}
 ?>
