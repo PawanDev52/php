0bc519c8d749a39663a475beef185a8611469dbb
Pawan sharma <pawansh803@gmail.com>
2024-08-26 18:34:12 +0530

    php file

---
+++ 20crud_operation.php
@@ -0,0 +1,24 @@
+<?php
+// connection to database with mysql
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "college";
+
+$conn = new mysqli($host, $username, $password, $database);
+// if(!$conn){
+//     die('we failed to connect');
+// }else{
+//     echo "Connection successful";
+// }
+
+if($conn->connect_error){
+    die("some error " . $conn->$connect_error);
+}
+echo "connection successful";
+echo "<br/>";
+
+$result = $conn->query('show tables')->fetch_all();
+print_r($result);
+
+?>
---
+++ 20crud_operation1.php
@@ -0,0 +1,24 @@
+<?php
+// connection to database with PDO
+
+$host = "localhost";
+$username = "root";
+$password = "";
+
+// PDO - PHP data object
+try{
+    $conn = new PDO("mysql:host=$host;dbname=college",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connection successful";
+}catch(PDOException $err){
+    echo "connection failed " . $err->getMessage();
+}
+
+echo "<br/>";
+
+$result = $conn->query('show tables');
+while($row = $result->fetch(PDO::FETCH_NUM)){
+    print_r($row);
+}
+
+?>
