620ae74e6cd2f8953cf1afaf6e06bba1afcc8cac
Pawan sharma <pawansh803@gmail.com>
2024-08-28 23:13:46 +0530

    php file

--- php_mysql/config.php
+++ php_mysql/config.php
@@ -1,4 +1,12 @@
 <?php
+// connection with PDO method
 
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "college";
+
+$conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
