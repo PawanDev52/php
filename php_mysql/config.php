48a6c1e5e5ec558f482cc468bda0a8cb369175ca
Pawan sharma <pawansh803@gmail.com>
2024-08-29 21:05:21 +0530

    php file

--- php_mysql/config.php
+++ php_mysql/config.php
@@ -7,6 +7,6 @@ $password = "";
 $database = "college";
 
 $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
-$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
