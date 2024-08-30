2f64d279d6a8beba3d3c1332e5356c0da8e9c62b
Pawan sharma <pawansh803@gmail.com>
2024-08-30 16:21:24 +0530

    php file

--- php_mysql/insert.php
+++ php_mysql/insert.php
@@ -1,4 +1,16 @@
 <?php
+// inserting data in mysql table with static data
+include('./config.php');
+$query = $conn->prepare("
+INSERT into `students` (`name`, `course`, `batch`, `city`, `year`)
+VALUES ('henry', 'bba', 'evening', 'patna', '3rd')
+");
 
-
+$result = $query->execute();
+if ($result) {
+    echo "Data inserted successfully";
+} else {
+    echo "Operation failed";
+}
 ?>
+
