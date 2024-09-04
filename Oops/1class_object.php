0eeb815e74d7d2e60a40d637169153529734c521
Pawan sharma <pawansh803@gmail.com>
2024-09-04 15:59:13 +0530

    php file

--- Oops/1class_object.php
+++ Oops/1class_object.php
@@ -1,8 +1,38 @@
-<?
+<?php
 // starting oops with php
 
-class MathOperations{
+class MathOperations
+{
+    // public $val=30; // used for creating variables in class
+    function sum($a, $b)
+    {
+        return $a + $b;
+    }
 
-};
+    function multi($a, $b)
+    {
+        echo $a * $b;
+    }
+
+    function sub($a, $b)
+    {
+        echo $a - $b;
+    }
+
+    function divi($a, $b)
+    {
+        echo $a / $b;
+    }
+}
+
+$math = new MathOperations();
+echo $math->sum(56, 67);
+echo "<br>";
+// echo $math->sum(46, 87);
+$math->multi(5, 4);
+
+// $math1 = new MathOperations();
+// echo "<br>";
+// echo $math1->sum(100, 600);
 
 ?>
