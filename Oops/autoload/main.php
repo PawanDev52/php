91a622135819b805a95aac4117ac8889c1fdc997
Pawan sharma <pawansh803@gmail.com>
2024-09-10 23:07:10 +0530

    php file

--- Oops/autoload/main.php
+++ Oops/autoload/main.php
@@ -4,13 +4,17 @@
 
 function autoLoader($class)
 {
-    echo $class . '.php';
+    // echo $class . '.php';
     echo "<br>";
     include($class . '.php');
 }
+
 // jis class ka object create hota hai vhi class include hoti hai
-spl_autoload_register('autoloader');
+
+spl_autoload_register('autoLoader'); // this is a special function which takes a function as a parameter (or parameter me function as a string jayega directly nhi)
 
 $t1 = new Teacher();
+$s1 = new Student();
+$m1 = new Management();
 
 ?>
