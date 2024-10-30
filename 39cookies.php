d8d55d372f2f697e1943f71cf9c95209ea2a754b
Pawan sharma <pawansh803@gmail.com>
2024-10-30 23:08:15 +0530

    php file

--- 39cookies.php
+++ 39cookies.php
@@ -1,24 +1,25 @@
 <?php
 // set cookies in php
 
-setcookie("fruit", "apple", time()+(86400));
-setcookie("color", "red", time()+(86400));
+setcookie("fruit", "apple", time() + (86400));
+setcookie("color", "red", time() + (86400));
 
-if(isset($_COOKIE['fruit'])){
+if (isset($_COOKIE['fruit'])) {
     echo "Current cookie is " . $_COOKIE['fruit'];
-}else{
+} else {
     echo "No cookies set";
 }
 
 echo "<br/>";
 
-if(isset($_COOKIE['color'])){
+if (isset($_COOKIE['color'])) {
     echo "Current cookie is " . $_COOKIE['color'];
-}else{
+} else {
     echo "No cookies set";
 }
 
 echo "<br/>";
 
 print_r($_COOKIE);
+
 ?>
