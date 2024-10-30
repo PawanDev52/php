89276725ace9676bbf1756f18d2d1d14252bb7c1
Pawan sharma <pawansh803@gmail.com>
2024-10-30 23:06:18 +0530

    php file

--- 41session.php
+++ 41session.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Session</title>
 </head>
+
 <body>
   <form action="" method="post">
     <input type="text" name="user" placeholder="Enter your name">
@@ -16,20 +18,21 @@
     <button name="button" value="delete">Delete session</button>
   </form>
 </body>
+
 </html>
 
 <?php
 session_start();
 if (isset($_POST['button'])) {
-    if ($_POST['button'] == 'set') {
-        $val = $_POST['user'];
-        $_SESSION['user'] = $val;
-    }
-    if ($_POST['button'] == 'get') {
-        echo $_SESSION['user'];
-    }
-    if ($_POST['button'] == 'delete') {
-        session_destroy();
-    }
+  if ($_POST['button'] == 'set') {
+    $val = $_POST['user'];
+    $_SESSION['user'] = $val;
+  }
+  if ($_POST['button'] == 'get') {
+    echo $_SESSION['user'];
+  }
+  if ($_POST['button'] == 'delete') {
+    session_destroy();
+  }
 }
 ?>
