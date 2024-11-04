c4dda721e25afc4fb50bffd90e9c01f9311010a2
Pawan sharma <pawansh803@gmail.com>
2024-11-04 23:02:38 +0530

    php file

--- 41session.php
+++ 41session.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Session</title>
 </head>
 <body>
@@ -20,14 +18,17 @@
 
 <?php
 session_start();
+
 if (isset($_POST['button'])) {
   if ($_POST['button'] == 'set') {
     $val = $_POST['user'];
     $_SESSION['user'] = $val;
   }
+
   if ($_POST['button'] == 'get') {
     echo $_SESSION['user'];
   }
+
   if ($_POST['button'] == 'delete') {
     session_destroy();
   }
