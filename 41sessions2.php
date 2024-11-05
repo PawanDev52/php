933052586b787e9a5ab5b8d0d56ef1975ed7816d
Pawan sharma <pawansh803@gmail.com>
2024-11-05 20:26:25 +0530

    php file

--- 41sessions2.php
+++ 41sessions2.php
@@ -1,5 +1,8 @@
 <?php
+session_start(); // for accessing session data we have to start the session here also
 
+echo "Welcome " . $_SESSION['username'];
 
+echo "<br> Your favourite category is " . $_SESSION['favCat'];
 
 ?>
