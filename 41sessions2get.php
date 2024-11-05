57fcb2c80ddabb955462b84f51dc8bfafa76893b
Pawan sharma <pawansh803@gmail.com>
2024-11-05 21:05:04 +0530

    php file

--- 41sessions2get.php
+++ 41sessions2get.php
@@ -3,8 +3,10 @@
 
 session_start(); // for accessing session data we have to start the session here also
 
-echo "Welcome " . $_SESSION['username'];
-
-echo "<br> Your favourite category is " . $_SESSION['favCat'];
-
+if (isset($_SESSION['username'])) {
+    echo "Welcome " . $_SESSION['username'];
+    echo "<br> Your favourite category is " . $_SESSION['favCat'];
+} else {
+    echo "Please login to continue";
+}
 ?>
--- 41sessions3dlt.php
+++ 41sessions3dlt.php
@@ -1,5 +1,12 @@
 <?php
 // delete session data
 
+session_start();
+ 
+session_unset(); // this will unset the session values
+
+session_destroy(); // this will destroy the session
+
+echo "<br> You have been logged out";
 
 ?>
